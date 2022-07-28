4286017ea5116dcb40104582042e47444b62d591
Callum McIntyre <callum@pointer.gg>
2022-07-28 17:19:33 +0100

    Add create_pixel instruction, not using PDA yet

--- draw-with-frens/programs/draw-with-frens/src/lib.rs
+++ draw-with-frens/programs/draw-with-frens/src/lib.rs
@@ -6,10 +6,90 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod draw_with_frens {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    const MIN_POS: u8 = 0;
+    const MAX_POS: u8 = 99;
+    const MIN_COL: u8 = 0;
+    const MAX_COL: u8 = 255;
+
+    pub fn create_pixel(
+        ctx: Context<CreatePixel>,
+        pos_x: u8,
+        pos_y: u8,
+        init_col_r: u8,
+        init_col_g: u8,
+        init_col_b: u8,
+    ) -> Result<()> {
+        // Validation
+        if pos_x < MIN_POS || pos_x > MAX_POS {
+            return Err(ErrorCode::InvalidXCoordinate.into());
+        }
+
+        if pos_y < MIN_POS || pos_y > MAX_POS {
+            return Err(ErrorCode::InvalidYCoordinate.into());
+        }
+
+        if init_col_r < MIN_COL || init_col_r > MAX_COL {
+            return Err(ErrorCode::InvalidRColor.into());
+        }
+
+        if init_col_b < MIN_COL || init_col_b > MAX_COL {
+            return Err(ErrorCode::InvalidBColor.into());
+        }
+
+        if init_col_g < MIN_COL || init_col_g > MAX_COL {
+            return Err(ErrorCode::InvalidGColor.into());
+        }
+
+        // Set values
+        let pixel = &mut ctx.accounts.pixel;
+        pixel.pos_x = pos_x;
+        pixel.pos_y = pos_y;
+        pixel.col_r = init_col_r;
+        pixel.col_g = init_col_g;
+        pixel.col_b = init_col_b;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePixel<'info> {
+    #[account(init, payer=user, space=Pixel::LEN)]
+    pub pixel: Account<'info, Pixel>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Define structure of pixel account
+#[account]
+pub struct Pixel {
+    pub pos_x: u8,
+    pub pos_y: u8,
+    pub col_r: u8,
+    pub col_g: u8,
+    pub col_b: u8,
+}
+
+// Define size of pixel account
+const DISCRIMINATOR_LENGTH: usize = 8;
+const POS_LENGTH: usize = 1;
+const COL_LENGTH: usize = 1;
+
+impl Pixel {
+    const LEN: usize = DISCRIMINATOR_LENGTH + (2 * POS_LENGTH) + (3 * COL_LENGTH);
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The given X co-ordinate is not between 0-99")]
+    InvalidXCoordinate,
+    #[msg("The given Y co-ordinate is not between 0-99")]
+    InvalidYCoordinate,
+    #[msg("The given R color is not between 0-255")]
+    InvalidRColor,
+    #[msg("The given G color is not between 0-255")]
+    InvalidGColor,
+    #[msg("The given B color is not between 0-255")]
+    InvalidBColor,
+}
--- draw-with-frens/tests/draw-with-frens.ts
+++ draw-with-frens/tests/draw-with-frens.ts
@@ -1,16 +1,55 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
+import { web3 } from "@project-serum/anchor";
 import { DrawWithFrens } from "../target/types/draw_with_frens";
+import { assert } from "chai"
 
 describe("draw-with-frens", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const anchorProvider = anchor.AnchorProvider.env();
+  anchor.setProvider(anchorProvider);
 
   const program = anchor.workspace.DrawWithFrens as Program<DrawWithFrens>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Can create a pixel", async () => {
+    const pixelKeypair = web3.Keypair.generate()
+
+    await program.methods
+      .createPixel(10, 10, 0, 0, 255)
+      .accounts({
+        pixel: pixelKeypair.publicKey,
+        user: anchorProvider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([pixelKeypair])
+      .rpc()
+
+    const storedPixel = await program.account.pixel.fetch(pixelKeypair.publicKey)
+    assert.equal(storedPixel.posX, 10)
+    assert.equal(storedPixel.posY, 10)
+    assert.equal(storedPixel.colR, 0)
+    assert.equal(storedPixel.colG, 0)
+    assert.equal(storedPixel.colB, 255)
   });
+
+  it("Does not allow creating a pixel out of bounds", async () => {
+    const pixelKeypair = web3.Keypair.generate()
+
+    await program.methods
+      .createPixel(0, 200, 0, 0, 255)
+      .accounts({
+        pixel: pixelKeypair.publicKey,
+        user: anchorProvider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([pixelKeypair])
+      .rpc()
+      .then(
+        () => Promise.reject(new Error('Expected to error!')),
+        (e: AnchorError) => {
+          // Log is eg. `AnchorError occurred. Error Code: InvalidYCoordinate. Error Number: 6001. Error Message: The given Y co-ordinate is not between 0-99.`
+          assert.ok(e.errorLogs.some(log => log.includes('InvalidYCoordinate') && log.includes('The given Y co-ordinate is not between 0-99.')))
+        }
+      );
+  })
 });
