e31139dc54d16cec43c2eb45629e809b2cd26a5f
Callum McIntyre <callum@pointer.gg>
2022-08-03 15:32:17 +0100

    Add Solana wallet connect

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,36 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
 import '../styles/index.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // You can also provide a custom RPC endpoint.
+  const endpoint = 'http://localhost:8899';
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+  ];
+
   return (
-    <>
-      <Head>
-        <title>Draw With Frens</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Head>
+            <title>Draw With Frens</title>
+          </Head>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Title from "../components/Title";
 import { Color, colors } from "../lib/colors";
 import { useState } from "react";
 import Canvas from "../components/Canvas";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function Home() {
   const [selectedColor, setSelectedColor] = useState<Color>(colors[0])
@@ -11,6 +12,10 @@ export default function Home() {
     <div className="flex flex-col items-stretch gap-8 px-4 pt-24 mx-auto max-w-max">
       <main className="flex flex-col gap-4">
         <Title>Draw With Frens</Title>
+
+        <div className="basis-1/4">
+          <WalletMultiButton className='!bg-gray-900 hover:scale-105' />
+        </div>
 
         <ColorSelector selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
 
