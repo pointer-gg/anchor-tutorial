bdfce58e86233e99566a5dc3ef9340845b4f13b1
Callum McIntyre <callum@pointer.gg>
2022-08-05 16:00:28 +0100

    Render the fetched pixel data on the canvas

--- frontend/components/Canvas.tsx
+++ frontend/components/Canvas.tsx
@@ -1,6 +1,6 @@
-import { Program } from "@project-serum/anchor";
+import { IdlAccounts, Program } from "@project-serum/anchor";
 import clsx from "clsx"
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { DrawWithFrens } from "../idl/draw_with_frens";
 import Pixel from "./Pixel"
 
@@ -8,19 +8,32 @@ interface Props {
   program?: Program<DrawWithFrens>
 }
 
+type PixelAccount = IdlAccounts<DrawWithFrens>['pixel']
+
 export default function Canvas({ program }: Props) {
   const disabled = !program;
+  const [fetchedPixels, setFetchedPixels] = useState<PixelAccount[]>([]);
 
   const fetchPixels = async () => {
     if (program) {
       const pixels = await program.account.pixel.all()
       console.log("got the pixels!", pixels)
+      setFetchedPixels(pixels.map(p => p.account))
     }
   }
 
   useEffect(() => {
     fetchPixels()
   }, [program])
+
+  const pixelsMap = useMemo(() => {
+    const map: { [id: number]: PixelAccount } = {};
+    fetchedPixels.forEach(p => {
+      const id = p.posY * 100 + p.posX;
+      map[id] = p;
+    })
+    return map
+  }, [fetchedPixels])
 
   return (
     <div className={clsx(disabled && "opacity-25 cursor-not-allowed")}>
@@ -30,11 +43,14 @@ export default function Canvas({ program }: Props) {
             return (
               <tr className="divide-x divide-gray-300" key={y}>
                 {[...Array(100)].map((_, x) => {
+                  const id = y * 100 + x;
+                  const pixelData = pixelsMap[id];
 
                   return <Pixel
                     posX={x}
                     posY={y}
                     program={program}
+                    pixelData={pixelData}
                     key={x}
                   />
                 })}
--- frontend/components/Pixel.tsx
+++ frontend/components/Pixel.tsx
@@ -1,15 +1,19 @@
-import { Program } from "@project-serum/anchor";
+import { IdlAccounts, Program } from "@project-serum/anchor";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { DrawWithFrens } from "../idl/draw_with_frens";
+
+type PixelAccount = IdlAccounts<DrawWithFrens>['pixel']
 
 interface Props {
   posX: number,
   posY: number,
   program: Program<DrawWithFrens>,
+  pixelData?: PixelAccount,
 }
 
-export default function Pixel({ posX, posY, program }: Props) {
-  const color = "white";
+export default function Pixel({ posX, posY, program, pixelData }: Props) {
+  const { colR, colG, colB } = pixelData || {};
+  const color = pixelData ? `rgb(${colR}, ${colG}, ${colB})` : "white"
 
   const getPixelAddress = () => {
     const [pixelPublicKey] = PublicKey.findProgramAddressSync(
